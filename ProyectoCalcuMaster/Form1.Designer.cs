d0e63ea63514a801f4c1fc450ec41df3b2add873
Arturo-Chi <124841476+Arturo-Chi@users.noreply.github.com>
2024-03-03 19:25:16 -0600

    Act1

---
+++ ProyectoCalcuMaster/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ProyectoCalcuMaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            listView1 = new ListView();
+            btn_tabla1 = new Button();
+            btn_tabla2 = new Button();
+            btn_tabla3 = new Button();
+            btn_tabla4 = new Button();
+            btn_tabla5 = new Button();
+            btn_tabla6 = new Button();
+            btn_menu = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Enabled = false;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(378, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(400, 78);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.UseWaitCursor = true;
+            // 
+            // listView1
+            // 
+            listView1.BackColor = Color.MidnightBlue;
+            listView1.Location = new Point(-1, -1);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(174, 592);
+            listView1.TabIndex = 8;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.UseWaitCursor = true;
+            // 
+            // btn_tabla1
+            // 
+            btn_tabla1.BackColor = Color.FromArgb(255, 224, 192);
+            btn_tabla1.BackgroundImage = (Image)resources.GetObject("btn_tabla1.BackgroundImage");
+            btn_tabla1.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_tabla1.FlatStyle = FlatStyle.Flat;
+            btn_tabla1.ForeColor = Color.NavajoWhite;
+            btn_tabla1.Location = new Point(251, 115);
+            btn_tabla1.Name = "btn_tabla1";
+            btn_tabla1.RightToLeft = RightToLeft.No;
+            btn_tabla1.Size = new Size(162, 203);
+            btn_tabla1.TabIndex = 9;
+            btn_tabla1.UseVisualStyleBackColor = false;
+            btn_tabla1.UseWaitCursor = true;
+            btn_tabla1.Click += button1_Click;
+            // 
+            // btn_tabla2
+            // 
+            btn_tabla2.BackgroundImage = (Image)resources.GetObject("btn_tabla2.BackgroundImage");
+            btn_tabla2.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_tabla2.FlatStyle = FlatStyle.Flat;
+            btn_tabla2.ForeColor = Color.NavajoWhite;
+            btn_tabla2.Location = new Point(482, 115);
+            btn_tabla2.Name = "btn_tabla2";
+            btn_tabla2.Size = new Size(162, 203);
+            btn_tabla2.TabIndex = 10;
+            btn_tabla2.UseVisualStyleBackColor = true;
+            btn_tabla2.UseWaitCursor = true;
+            // 
+            // btn_tabla3
+            // 
+            btn_tabla3.BackgroundImage = (Image)resources.GetObject("btn_tabla3.BackgroundImage");
+            btn_tabla3.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_tabla3.FlatStyle = FlatStyle.Flat;
+            btn_tabla3.ForeColor = Color.NavajoWhite;
+            btn_tabla3.Location = new Point(720, 115);
+            btn_tabla3.Name = "btn_tabla3";
+            btn_tabla3.Size = new Size(162, 203);
+            btn_tabla3.TabIndex = 11;
+            btn_tabla3.UseVisualStyleBackColor = true;
+            btn_tabla3.UseWaitCursor = true;
+            // 
+            // btn_tabla4
+            // 
+            btn_tabla4.BackgroundImage = (Image)resources.GetObject("btn_tabla4.BackgroundImage");
+            btn_tabla4.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_tabla4.FlatStyle = FlatStyle.Flat;
+            btn_tabla4.ForeColor = Color.NavajoWhite;
+            btn_tabla4.Location = new Point(251, 353);
+            btn_tabla4.Name = "btn_tabla4";
+            btn_tabla4.Size = new Size(162, 203);
+            btn_tabla4.TabIndex = 12;
+            btn_tabla4.UseVisualStyleBackColor = true;
+            btn_tabla4.UseWaitCursor = true;
+            // 
+            // btn_tabla5
+            // 
+            btn_tabla5.BackgroundImage = (Image)resources.GetObject("btn_tabla5.BackgroundImage");
+            btn_tabla5.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_tabla5.FlatStyle = FlatStyle.Flat;
+            btn_tabla5.ForeColor = Color.NavajoWhite;
+            btn_tabla5.Location = new Point(482, 353);
+            btn_tabla5.Name = "btn_tabla5";
+            btn_tabla5.Size = new Size(162, 203);
+            btn_tabla5.TabIndex = 13;
+            btn_tabla5.UseVisualStyleBackColor = true;
+            btn_tabla5.UseWaitCursor = true;
+            // 
+            // btn_tabla6
+            // 
+            btn_tabla6.BackgroundImage = (Image)resources.GetObject("btn_tabla6.BackgroundImage");
+            btn_tabla6.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_tabla6.FlatStyle = FlatStyle.Flat;
+            btn_tabla6.ForeColor = Color.NavajoWhite;
+            btn_tabla6.Location = new Point(720, 365);
+            btn_tabla6.Name = "btn_tabla6";
+            btn_tabla6.Size = new Size(162, 203);
+            btn_tabla6.TabIndex = 14;
+            btn_tabla6.UseVisualStyleBackColor = true;
+            btn_tabla6.UseWaitCursor = true;
+            // 
+            // btn_menu
+            // 
+            btn_menu.BackColor = Color.MidnightBlue;
+            btn_menu.BackgroundImage = (Image)resources.GetObject("btn_menu.BackgroundImage");
+            btn_menu.BackgroundImageLayout = ImageLayout.Stretch;
+            btn_menu.FlatStyle = FlatStyle.Flat;
+            btn_menu.ForeColor = Color.MidnightBlue;
+            btn_menu.Location = new Point(47, 41);
+            btn_menu.Name = "btn_menu";
+            btn_menu.Size = new Size(80, 49);
+            btn_menu.TabIndex = 15;
+            btn_menu.UseVisualStyleBackColor = false;
+            btn_menu.UseWaitCursor = true;
+            btn_menu.Click += btn_menu_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.NavajoWhite;
+            ClientSize = new Size(959, 593);
+            Controls.Add(btn_menu);
+            Controls.Add(btn_tabla6);
+            Controls.Add(btn_tabla5);
+            Controls.Add(btn_tabla4);
+            Controls.Add(btn_tabla3);
+            Controls.Add(btn_tabla2);
+            Controls.Add(btn_tabla1);
+            Controls.Add(listView1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Multimaster";
+            UseWaitCursor = true;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private ListView listView1;
+        private Button btn_tabla1;
+        private Button btn_tabla2;
+        private Button btn_tabla3;
+        private Button btn_tabla4;
+        private Button btn_tabla5;
+        private Button btn_tabla6;
+        private Button btn_menu;
+    }
+}
---
+++ ProyectoCalcuMaster/Form1.cs
@@ -0,0 +1,30 @@
+namespace ProyectoCalcuMaster
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabla1_img_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_menu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
